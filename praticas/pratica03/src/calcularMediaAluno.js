6248b3e0f8cba91c557f009395850d215add8ad3
Samuel <samuelsmagalhaes2004@gmail.com>
2025-09-02 21:33:15 -0300

    Pratica 03

--- praticas/pratica03/src/calcularMediaAluno.js
+++ praticas/pratica03/src/calcularMediaAluno.js
@@ -7,6 +7,7 @@ function calcularMediaAluno(a1, a2, a3) {
     throw Error("Nota a2 não informada");
   }
 
+
   if (a1 < 0 && a2 < 0) {
     throw Error("Notas a1 ou a2 não podem ser negativas");
   } else if (a1 < 0) {
@@ -15,14 +16,17 @@ function calcularMediaAluno(a1, a2, a3) {
     throw Error("Nota a2 não pode ser negativa");
   }
 
+
   if (a3 == undefined) {
     return a1 * 0.4 + a2 * 0.6;
   }
+
 
   if (a3 < 0) {
     throw Error("Nota a3 não pode ser negativa");
   }
 
+  
   if (a1 + a3 > a1 + a2) {
     return a1 * 0.4 + a3 * 0.6;
   } else if(a2 + a3 > a1 + a2){
