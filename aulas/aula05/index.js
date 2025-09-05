2b7116747cf5e9da3a766fdfc4ff96d713e4a461
Samuel <malakaigamer5@gmail.com>
2025-09-04 21:45:00 -0300

    Middlewares

---
+++ aulas/aula05/index.js
@@ -0,0 +1,44 @@
+// importa o framework
+const express = require("express");
+
+// cria uma instância da aplicação
+const app = express();
+
+//middleware de aplicação
+app.use((req, res, next) => {
+  console.log("Passei aqui");
+  next();
+});
+
+//middleware de rota
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Chegou aqui");
+});
+
+router.post("/", (req, res) => {
+  res.status(201).send("Inserido com sucesso");
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  if (id == 1) {
+    return res.send("Achei");
+  } else {
+    throw Error("Não achei");
+  }
+});
+
+app.use("/tarefas", router);
+
+// middleware de erro
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).send("Algo de errado não está certo!")
+});
+
+// inicia a aplicação
+app.listen(3000, () => {
+  console.log("App está ON!");
+});
